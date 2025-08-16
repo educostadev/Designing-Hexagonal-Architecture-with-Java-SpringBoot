efb1b520d9aded03724f8fe0a5a32e4f787edcb5
Edu Costa <educostadev@gmail.com>
2025-08-16 20:33:56 -0300

    Fixed unit test

--- bootstrap/src/test/java/dev/educosta/ApplicationTests.java
+++ bootstrap/src/test/java/dev/educosta/ApplicationTests.java
@@ -2,9 +2,19 @@ package dev.educosta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import dev.educosta.framework.adapters.jpa.CityRepository;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+	DataSourceAutoConfiguration.class
+})
 class ApplicationTests {
+
+	@MockBean
+	private CityRepository cityRepository;
 
 	@Test
 	void contextLoads() {
